a419a7bf8dfa672e142f2ddc6cc48211cb96fb17
Mujeeb Sulayman <sulaymanmujeeb6@Gmail.com>
2024-02-28 04:58:14 +0100

    ..

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,8 +1,22 @@
+import Image from 'next/image';
 import React from 'react'
 
 const TransformedImage = ({title, image, type, transformationConfig, setIsTransforming,isTransforming, hasDownload = false}: TransformedImageProps) => {
+
+  const downloadHandler = () => {};
   return (
-    <div>TranformedImage</div>
+    <div className='flex flex-col gap-4'>
+      <div className='flex-between'>
+        <h3 className='h3-bold text-dark-600'>
+          After
+        </h3>
+        {hasDownload && (
+          <button className='download-btn' onClick={downloadHandler}>
+           <Image src='/assets/icons/download.svg' alt='download' width='24' height='24' className='pb-[6px]'/>
+          </button>
+        )}
+      </div>
+    </div>
   )
 }
 
