b17e36848d42bbb482dd49e6bfb978105133f687
Mujeeb Sulayman <sulaymanmujeeb6@Gmail.com>
2024-02-28 17:03:51 +0100

    Done

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,73 +1,91 @@
-import { dataUrl, debounce, getImageSize } from '@/lib/utils';
-import { CldImage } from 'next-cloudinary';
+'use client';
+
+import { dataUrl, debounce, download, getImageSize } from '@/lib/utils';
+import { CldImage, getCldImageUrl } from 'next-cloudinary';
 import { PlaceholderValue } from 'next/dist/shared/lib/get-img-props';
 import Image from 'next/image';
 import React from 'react';
 
 const TransformedImage = ({
-	title,
 	image,
 	type,
+	title,
 	transformationConfig,
-	setIsTransforming,
 	isTransforming,
+	setIsTransforming,
 	hasDownload = false,
 }: TransformedImageProps) => {
-	const downloadHandler = () => {};
+	const downloadHandler = (
+		e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+	) => {
+		e.preventDefault();
+
+		download(
+			getCldImageUrl({
+				width: image?.width,
+				height: image?.height,
+				src: image?.publicId,
+				...transformationConfig,
+			}),
+			title
+		);
+	};
+
 	return (
 		<div className='flex flex-col gap-4'>
 			<div className='flex-between'>
-				<h3 className='h3-bold text-dark-600'>After</h3>
+				<h3 className='h3-bold text-dark-600'>Transformed</h3>
+
 				{hasDownload && (
 					<button
 						className='download-btn'
 						onClick={downloadHandler}>
 						<Image
 							src='/assets/icons/download.svg'
-							alt='download'
-							width='24'
-							height='24'
+							alt='Download'
+							width={24}
+							height={24}
 							className='pb-[6px]'
 						/>
 					</button>
 				)}
 			</div>
-			{image?.public_id && transformationConfig ? (
+
+			{image?.publicId && transformationConfig ? (
 				<div className='relative'>
 					<CldImage
 						width={getImageSize(type, image, 'width')}
 						height={getImageSize(type, image, 'height')}
-						src={image?.public_id}
+						src={image?.publicId}
 						alt={image.title}
 						sizes={'(max-width: 767px) 100vw, 50vw'}
 						placeholder={dataUrl as PlaceholderValue}
 						className='transformed-image'
 						onLoad={() => {
-							debounce(() => {
-								setIsTransforming && setIsTransforming(false);
-							}, 8000);
-						}}
-						onError={() => {
 							setIsTransforming && setIsTransforming(false);
 						}}
-
+						onError={() => {
+							debounce(() => {
+								setIsTransforming && setIsTransforming(false);
+							}, 8000)();
+						}}
 						{...transformationConfig}
 					/>
+
 					{isTransforming && (
 						<div className='transforming-loader'>
-							<Image 
-							src='/assets/icons/spinner.svg'
-							width={50}
-							height={50}
-							alt='Tranforming'
+							<Image
+								src='/assets/icons/spinner.svg'
+								width={50}
+								height={50}
+								alt='spinner'
 							/>
+							<p className='text-white/80'>Please wait...</p>
 						</div>
 					)}
 				</div>
 			) : (
-				<div className='transformed-placeholder'>
-					
-				</div>
+				<div className='transformed-placeholder'>Transformed Image</div>
 			)}
 		</div>
 	);
