71f0b3cff86978495b33ce5753571ed92a05e066
Mujeeb Sulayman <sulaymanmujeeb6@Gmail.com>
2024-03-04 20:29:35 +0100

    ....

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,14 @@ export default authMiddleware({
 	publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'],
 });
 
+const ignoredRoutes = [
+	'/((?!api|trpc))(_next.*|.+.[w]+$)',
+	'/transformations/add/remove',
+];
 export const config = {
 	matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+const middleware = authMiddleware({
+	ignoredRoutes: ignoredRoutes,
+});
